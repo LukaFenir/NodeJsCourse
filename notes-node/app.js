3b44b4b6c9e94d66dbe5b0fdbeb2791b88a3aee5
LukaFenir <laura@nichols-online.co.uk>
2018-01-08 23:16:59 +0000

    13: Getting input. Failed 12: Nodemon

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,14 +1,21 @@
 console.log("Starting app.");
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('Banana'));
-var filteredArray = _.uniq(['Banana','Banana',1,1,2,3,4]);
-console.log(filteredArray);
-// var user = os.userInfo();
+var command = process.argv[2];
+console.log('Command: ',command);
 
-// fs.appendFileSync('greetings.txt',`\nHello ${user.username}! You are ${notes.age}! Old as fuck!!!`,)
+if(command ==='add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes')
+} else if (command === 'read') {
+    console.log('Reading note')
+} else if (command === 'remove') {
+    console.log('Note removed')
+} else {
+    console.log('Command not recognised');
+}
